30b0a57e9c0685363311b74e87d6368581af1cc9
honestynabila12 <57379335+honestynabila12@users.noreply.github.com>
2019-11-05 10:01:22 +0700

    contoh

---
+++ src/PegadaianGitHub/Main.java
@@ -0,0 +1,12 @@
+package PegadaianGitHub;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hari ini belajar GitHub");
+		System.out.println("Honesty Nabila Y");
+
+	}
+
+}
