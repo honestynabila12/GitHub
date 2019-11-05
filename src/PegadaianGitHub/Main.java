16517b51a419a6c7699f5271b4afa4f9ccd12d20
honestynabila12 <57379335+honestynabila12@users.noreply.github.com>
2019-11-05 11:40:32 +0700

    Hanry
    Hanry

--- src/PegadaianGitHub/Main.java
+++ src/PegadaianGitHub/Main.java
@@ -4,8 +4,8 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hari ini belajar Git");
-		System.out.println("Honesty ");
+		System.out.println("Hanry");
+		System.out.println("Hanry ");
 		
 	}
 
